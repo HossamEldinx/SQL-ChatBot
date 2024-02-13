4f22d6c1273e167a903bf1d1136a8102f10da09a
hossamElden92 <77191082+hossamElden92@users.noreply.github.com>
2024-02-13 18:08:58 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["typeorm"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
